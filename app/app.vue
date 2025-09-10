2fa08e642ac8d9bae50b454cd6c84e5480ccc67a
tonight80 <m1sty32@yandex.ru>
2025-09-10 18:49:13 +0300

    страница abiturent

--- app/app.vue
+++ app/app.vue
@@ -1,27 +1,26 @@
 <template>
   <SiteHeader />
-
   <NuxtLoadingIndicator color="#00ffdd" :height="3" />
-
   <NuxtLayout>
     <ErrorPage v-if="showCustomError" />
     <NuxtPage v-else />
   </NuxtLayout>
-
   <SiteFooter />
-
   <CustomLoader v-if="initialLoading" />
+  <ApplyModal v-if="isModalOpen" @close="isModalOpen = false" />
 </template>
 
 <script setup lang="ts">
-import { onErrorCaptured, ref, onMounted } from 'vue'
+import { onErrorCaptured, onMounted, ref } from 'vue'
+import CustomLoader from '~/components/CustomLoader.vue'
 import SiteFooter from '~/components/SiteFooter.vue'
 import SiteHeader from '~/components/SiteHeader.vue'
 import ErrorPage from '~/error.vue'
-import CustomLoader from '~/components/CustomLoader.vue'
+import ApplyModal from '~/components/ui/ApplyModal.vue'
 
 const showCustomError = ref(false)
 const initialLoading = ref(true)
+const isModalOpen = ref(false)
 
 onErrorCaptured((err) => {
   console.error('Captured error:', err)
@@ -34,4 +33,6 @@ onMounted(() => {
     initialLoading.value = false
   }, 1500)
 })
+
+defineExpose({ isModalOpen })
 </script>
