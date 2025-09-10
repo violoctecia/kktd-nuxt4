8507bcfa96b49b23246b70a0faf967efb910305c
tonight80 <m1sty32@yandex.ru>
2025-09-10 18:11:00 +0300

    loader custom

--- app/app.vue
+++ app/app.vue
@@ -1,23 +1,37 @@
 <template>
   <SiteHeader />
+
+  <NuxtLoadingIndicator color="#00ffdd" :height="3" />
+
   <NuxtLayout>
     <ErrorPage v-if="showCustomError" />
     <NuxtPage v-else />
   </NuxtLayout>
+
   <SiteFooter />
+
+  <CustomLoader v-if="initialLoading" />
 </template>
 
 <script setup lang="ts">
-import { ref, onErrorCaptured } from 'vue'
+import { onErrorCaptured, ref, onMounted } from 'vue'
 import SiteFooter from '~/components/SiteFooter.vue'
 import SiteHeader from '~/components/SiteHeader.vue'
 import ErrorPage from '~/error.vue'
+import CustomLoader from '~/components/CustomLoader.vue'
 
 const showCustomError = ref(false)
+const initialLoading = ref(true)
 
 onErrorCaptured((err) => {
   console.error('Captured error:', err)
   showCustomError.value = true
-  return false 
+  return false
+})
+
+onMounted(() => {
+  setTimeout(() => {
+    initialLoading.value = false
+  }, 1500)
 })
 </script>
--- app/error.vue
+++ app/error.vue
@@ -1,19 +1,12 @@
 <script setup lang="ts">
-import errorSvg from '~/assets/svg/original/404.svg'
+import errorSvg from '~/assets/svg/original/404.svg';
 </script>
 
 <template>
-  <div class="flex min-h-screen flex-col items-center justify-center bg-[#0E0E10] px-4">
-    <img
-      :src="errorSvg"
-      alt="404 error"
-      class="max-w-full w-[500px] h-auto mb-8"
-    />
-    <NuxtLink
-      to="/"
-      class="rounded-2xl bg-primary px-6 py-3 text-white text-lg font-medium shadow-lg hover:bg-primaryDark transition"
-    >
-      На главную
-    </NuxtLink>
-  </div>
+    <div class="flex min-h-screen flex-col items-center justify-center bg-[#0E0E10] px-4">
+        <img :src="errorSvg" alt="404 error" class="mb-8 h-auto w-[500px] max-w-full" />
+        <NuxtLink to="/" class="rounded-2xl bg-primary px-6 py-3 text-lg font-medium text-white shadow-lg transition hover:bg-primaryDark">
+            На главную
+        </NuxtLink>
+    </div>
 </template>
