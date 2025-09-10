13312d0e41347939d537aba0f687e2c3f3ac882d
tonight80 <m1sty32@yandex.ru>
2025-09-10 10:03:18 +0300

    faq

---
+++ app/components/sections/ContactInfo.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="bg-primary-light text-primary-dark rounded-2xl p-6 shadow-md mt-10">
+    <h3 class="text-xl font-semibold mb-4">Контактная информация</h3>
+    <div class="space-y-2">
+      <p><span class="font-medium">📍 Адрес:</span> г. Казань, ул. Шигабутдина Марджани, д. 26</p>
+      <p><span class="font-medium">📞 Телефон:</span> <a href="tel:+78432042970" class="hover:underline">+7 (843) 204-29-70</a></p>
+      <p><span class="font-medium">✉️ Email:</span> <a href="mailto:ktlp1@yandex.ru" class="hover:underline">ktlp1@yandex.ru</a></p>
+      <p><span class="font-medium">🕘 Время работы:</span> Пн–Пт, с 9:00 до 17:00</p>
+    </div>
+  </section>
+</template>
+
+<script lang="ts" setup>
+</script>
---
+++ app/components/sections/FaqItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-white rounded-xl p-8 shadow-lg border-l-4 border-primary">
+    <h3 class="faq-question text-primary-dark text-lg font-semibold mb-4">{{ question }}</h3>
+    <div class="faq-answer text-gray-700 leading-relaxed">
+      <p v-if="answerText">{{ answerText }}</p>
+      <ul v-if="answerList" class="ml-6 list-disc mt-2 space-y-1">
+        <li v-for="(li, liIndex) in answerList" :key="liIndex">{{ li }}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+interface FAQProps {
+  question: string
+  answerText?: string
+  answerList?: string[]
+}
+const props = defineProps<FAQProps>()
+</script>
