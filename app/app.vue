4dbc8e09da569eda6beab509d139189c623d39ca
tonight80 <m1sty32@yandex.ru>
2025-09-10 17:53:20 +0300

    page 404

--- app/app.vue
+++ app/app.vue
@@ -1,12 +1,23 @@
 <template>
-    <SiteHeader />
-    <NuxtLayout>
-        <NuxtPage />
-    </NuxtLayout>
-    <SiteFooter />
+  <SiteHeader />
+  <NuxtLayout>
+    <ErrorPage v-if="showCustomError" />
+    <NuxtPage v-else />
+  </NuxtLayout>
+  <SiteFooter />
 </template>
 
 <script setup lang="ts">
-import SiteFooter from '~/components/SiteFooter.vue';
-import SiteHeader from '~/components/SiteHeader.vue';
+import { ref, onErrorCaptured } from 'vue'
+import SiteFooter from '~/components/SiteFooter.vue'
+import SiteHeader from '~/components/SiteHeader.vue'
+import ErrorPage from '~/error.vue'
+
+const showCustomError = ref(false)
+
+onErrorCaptured((err) => {
+  console.error('Captured error:', err)
+  showCustomError.value = true
+  return false 
+})
 </script>
