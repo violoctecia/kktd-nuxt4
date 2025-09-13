aad9d982b98e8f3507e7db0fdfd2ac878bf3272f
GathanKelleri <97847064+GathanKelleri@users.noreply.github.com>
2025-09-13 14:37:53 +0300

    Add raiting page

---
+++ app/pages/rating.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Rating from '@/components/sections/Rating.vue';
+import SiteBreadcrumbs from '~/components/SiteBreadcrumbs.vue';
+
+const breadcrumbs = [{ label: 'Главная', href: '/' }, { label: 'Рейтинг абитуриентов' }];
+</script>
+
+<template>
+  <main>
+    <SiteBreadcrumbs :items="breadcrumbs" />
+    <div class="container mx-auto py-6">
+      <Rating />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.container { max-width: 1200px; }
+</style>
