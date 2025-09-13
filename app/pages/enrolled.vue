3020fcb967e585ac70a8df90a4fc01e83bb51118
GathanKelleri <97847064+GathanKelleri@users.noreply.github.com>
2025-09-13 14:30:28 +0300

    Re-Upload Enrolled and FIX

---
+++ app/pages/enrolled.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Enrolled from '@/components/sections/Enrolled.vue';
+import SiteBreadcrumbs from '~/components/SiteBreadcrumbs.vue';
+
+const breadcrumbs = [{ label: 'Главная', href: '/' }, { label: 'Зачисленные студенты' }];
+</script>
+
+<template>
+  <main>
+    <SiteBreadcrumbs :items="breadcrumbs" />
+    <div class="container mx-auto py-6">
+      <Enrolled />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.container { max-width: 1000px; }
+</style>
